73a5358dc879787448a6dfb1f1974a425c83d693
Jakub Sukiennik <kuba1207710@gmail.com>
2021-03-16 20:08:03 +0100

    adding morgan middleware

--- src/pages/api/[[...slug]].ts
+++ src/pages/api/[[...slug]].ts
@@ -1,7 +1,9 @@
 import { errorHandler, dbMiddleware } from '@api'
+import morgan from 'morgan'
 import nc from 'next-connect'
 
 const handler = nc({ attachParams: true, onError: errorHandler })
+  .use(morgan('dev'))
   .use(dbMiddleware)
   .get('/api', (req, res) => {
     res.json({ message: 'Working' })
